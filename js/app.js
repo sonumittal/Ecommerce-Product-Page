39cfad3674e892aa6c1852faf98accab4a545afb
sonumittal <sonu.mittal144@gmail.com>
2022-01-03 04:06:31 +0530

    JS handle next/previous arrow click on Mobile screen for lightbox

--- js/app.js
+++ js/app.js
@@ -64,3 +64,43 @@ function onThumbClick(event) {
     //update hero image
     heroImg.src = event.target.src.replace('-thumbnail', '');//--replacing -thumbnail with '' 
 }
+
+// handle next/previous arrow click on Mobile screen for lightbox
+const btnNext = document.querySelector('.next');
+const btnPrevious = document.querySelector('.previous');
+
+btnNext.addEventListener('click', handleBtnClickNext);
+btnPrevious.addEventListener('click', handleBtnClickPrevious);
+
+function handleBtnClickNext() {
+    let imageIndex = getCurrentImageIndex();
+    imageIndex++;
+    if (imageIndex > 4) {
+        imageIndex = 1;
+    }
+    setHeroImage(imageIndex);
+}
+
+function handleBtnClickPrevious() {
+    let imageIndex = getCurrentImageIndex();
+    imageIndex--;
+    if (imageIndex < 1) {
+        imageIndex = 4;
+    }
+    setHeroImage(imageIndex);
+}
+
+function getCurrentImageIndex() {
+    const imageIndex = parseInt(heroImg.src.split('\\').pop().split('/').pop().replace('.jpg', '').replace('image-product-', ''));
+    return imageIndex;
+}
+
+function setHeroImage(imageIndex) {
+    heroImg.src = `./images/image-product-${imageIndex}.jpg`;
+    //images are not sync
+    gallery.forEach(img => {
+        img.classList.remove('active');
+    });
+    //set active thumbnail
+    gallery[imageIndex-1].classList.add('active');
+}
