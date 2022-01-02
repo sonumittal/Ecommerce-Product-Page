99dd2ac3ff357c06028cc08a82ffa36d465a6d5d
sonumittal <sonu.mittal144@gmail.com>
2022-01-03 02:50:29 +0530

    JS product counter plus minus button features

--- js/app.js
+++ js/app.js
@@ -20,3 +20,31 @@ btnCart.addEventListener('click', openCart);
 function openCart() {
     cart.classList.toggle('hidden');
 }
+
+// product counter plus minus button features
+const btnPlus = document.querySelector('#btnPlus');
+const btnMinus = document.querySelector('#btnMinus');
+const productCounter = document.querySelector('.counter');
+
+btnPlus.addEventListener('click', productCounterPlus);
+btnMinus.addEventListener('click', productCounterMinus);
+
+//Numerical Variables
+let productCounterValue = 1;
+
+function productCounterPlus() {
+    //console.log(productCounterValue);
+    setProductCounter(1);
+}
+
+function productCounterMinus() {
+    setProductCounter(-1);
+}
+
+function setProductCounter(value) {
+    if ((productCounterValue + value) > 0) {
+        productCounterValue += value;
+        productCounter.innerHTML = productCounterValue;
+    }
+    //console.log(value);
+}
