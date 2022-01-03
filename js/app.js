755f772241253b4a03db21cdaa65cc5b701c850d
sonumittal <sonu.mittal144@gmail.com>
2022-01-03 05:34:54 +0530

    JS adding products to cart by button count feature

--- js/app.js
+++ js/app.js
@@ -104,3 +104,98 @@ function setHeroImage(imageIndex) {
     //set active thumbnail
     gallery[imageIndex-1].classList.add('active');
 }
+
+// adding products to cart by button count feature
+let productsInCart = 0;
+let price = 250.0
+let discount = 0.5;
+
+const btnAddToCard = document.querySelector('.btn');
+const cartCount = document.querySelector('.cart-count');//--cart icon value update
+const productInShoppingCart = document.querySelector('.products-in-cart');
+const msgEmpty = document.querySelector('.msg-empty');
+const checkout = document.querySelector('.checkout');
+
+btnAddToCard.addEventListener('click', addToCart);
+
+function addToCart() {
+    productsInCart += productCounterValue;
+
+    const productHTMLElement = `
+    <div class="item">
+        <img class="product-img" src="./images/image-product-1-thumbnail.jpg" alt="product 1 thumb">
+        <div class="details">
+            <div class="product-name">Autumn Limited Edition...</div>
+            <div class="price-group">
+                <div class="price">$${(price*discount).toFixed(2)}</div> x
+                <div class="count">${productsInCart}</div>
+                <div class="total-amount">$${(price*discount*productsInCart).toFixed(2)}</div>
+        </div>
+        </div>
+        <img id="btnDelete" src="./images/icon-delete.svg" alt="icon delete">
+    </div>
+    `;
+
+    productInShoppingCart.innerHTML = productHTMLElement;
+
+    updateCart();
+
+    const btnDelete = document.querySelector('#btnDelete');
+    btnDelete.addEventListener('click', onBtnDeleteClick);
+    //console.log(productsInCart);
+}
+
+function updateCart() {
+    updateCartIcon();
+    updateMsgEmpty();
+    updateCheckoutButton();
+}
+
+function updateCartIcon() {
+    cartCount.textContent = productsInCart;
+    if (productsInCart == 0) {
+        if (!cartCount.classList.contains('hidden')) {
+            cartCount.classList.add('hidden');
+        }
+    } else {
+        cartCount.classList.remove('hidden');
+    }
+}
+
+function updateMsgEmpty() {
+    if (productsInCart == 0) {
+        if (msgEmpty.classList.contains('hidden')) {
+            msgEmpty.classList.remove('hidden');
+        }
+    } else {
+        if (!msgEmpty.classList.contains('hidden')){
+            msgEmpty.classList.add('hidden');
+        }
+    }
+
+}
+
+function updateCheckoutButton() {
+    if (productsInCart == 0) {
+        if (!checkout.classList.contains('hidden')) {
+            checkout.classList.add('hidden');
+        }
+    } else {
+        checkout.classList.remove('hidden');
+    }
+}
+
+function onBtnDeleteClick() {
+    productsInCart--;
+    updateCart();
+
+    const el = document.querySelector('.count');
+    const totalAmount = document.querySelector('.total-amount');
+    el.innerHTML = productsInCart;
+    totalAmount.innerHTML = `$${(price*discount*productsInCart).toFixed(2)}`;
+
+    if (productsInCart == 0) {
+        productInShoppingCart.innerHTML = '';
+    }
+}
+
